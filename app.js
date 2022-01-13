dbd810ea8c9cfc9650d9c8c3897a0e0eb7b4c044
Tomas Valkendorff <Tomvalken1@gmail.com>
2022-01-13 16:14:50 +0200

    pay loans button and new button styling

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const elements = document.getElementsByClassName('element');
 
 //gets the buttons from the elements
 const loanButton = elements.item(0).querySelector('#loan');
+const payLoanButton = elements.item(0).querySelector('#payLoan');
 const bankButton = elements.item(1).querySelector('#bank');
 const workButton = elements.item(1).querySelector('#work');
 
@@ -19,9 +20,9 @@ let loanAmount = 0;
 let payBalance = 0;
 
 //functions for the buttons
-const LoanButtonFunc = () =>{
+const LoanButtonFunc = () => {
     //checks if there is a loan already and if so it skips
-    if (!loan){
+    if (!loan) {
         loanAmount = balance / 2;
         balance += loanAmount;
         loan = true;
@@ -29,19 +30,54 @@ const LoanButtonFunc = () =>{
     UpdateFields();
 };
 
-const BankButtonFunc = () =>{
+//adds value from pay blance to bank balance
+const BankButtonFunc = () => {
+    //checks for loans
+    if (loan) {
+        let tenPrecent = payBalance * 0.1;
+        //if 10% of the salary is less than loan amount
+        if (tenPrecent < loanAmount) {
+            loanAmount -= tenPrecent;
+            payBalance -= tenPrecent;
+        } else {
+            payBalance -= loanAmount;
+            loan = false;
+            loanAmount = 0;
+
+        }
+    }
     balance += payBalance;
     payBalance = 0;
     UpdateFields();
 };
 
-const WorkButtonFunc = () =>{
+//adds 100 to work balance
+const WorkButtonFunc = () => {
     payBalance += 100;
     UpdateFields();
 };
 
+//pays the loan in full
+const PayLoanFunc = () => {
+    //checks for loans
+    if (loan) {
+        let restOfLoan = loanAmount - payBalance;
+        //if you can't pay the loan in full
+        if (restOfLoan > 0) {
+            loanAmount -= payBalance;
+            payBalance = 0;
+        //if you can pay the loan in full it leaves rest of the money to your work balance
+        } else {
+            payBalance = -restOfLoan;
+            loan = false;
+            loanAmount = 0;
+        }
+    }
+    UpdateFields();
+}
+
 //updates the fields on the site to match the correct values
-const UpdateFields = () =>{
+const UpdateFields = () => {
     balanceField.textContent = balance;
     loanField.textContent = loanAmount;
     payField.textContent = payBalance
@@ -50,5 +86,6 @@ const UpdateFields = () =>{
 loanButton.onclick = LoanButtonFunc;
 bankButton.onclick = BankButtonFunc;
 workButton.onclick = WorkButtonFunc;
+payLoanButton.onclick = PayLoanFunc;
 
 UpdateFields();
