a7bfec5d1df56d8dca0ce9a072b5ed757e04f58f
Tomas Valkendorff <Tomvalken1@gmail.com>
2022-01-13 15:11:23 +0200

    buttons now work and there is a decent grid layout

--- app.js
+++ app.js
@@ -2,20 +2,53 @@
 const elements = document.getElementsByClassName('element');
 
 //gets the buttons from the elements
-const loanB = elements.item(0).querySelector('#loan');
-const bankB = elements.item(1).querySelector('#bank');
-const workB = elements.item(2).querySelector('#work');
+const loanButton = elements.item(0).querySelector('#loan');
+const bankButton = elements.item(1).querySelector('#bank');
+const workButton = elements.item(1).querySelector('#work');
 
-const balance = document.getElementById('balance');
+//gets the fields that show info
+const balanceField = elements.item(0).querySelector('#balance');
+const loanField = elements.item(0).querySelector('#loanAmount');
+const payField = elements.item(1).querySelector('#pay');
 
-const LoanButton = () =>{
-    console.log("woo");
-};
-const BankButton = () =>{
-    
-};
-const WorkButton = () =>{
-    
+//data storage
+let balance = 0;
+let loan = false;
+let loanAmount = 0;
+
+let payBalance = 0;
+
+//functions for the buttons
+const LoanButtonFunc = () =>{
+    //checks if there is a loan already and if so it skips
+    if (!loan){
+        loanAmount = balance / 2;
+        balance += loanAmount;
+        loan = true;
+    }
+    UpdateFields();
 };
 
-loanB.onclick = LoanButton;
+const BankButtonFunc = () =>{
+    balance += payBalance;
+    payBalance = 0;
+    UpdateFields();
+};
+
+const WorkButtonFunc = () =>{
+    payBalance += 100;
+    UpdateFields();
+};
+
+//updates the fields on the site to match the correct values
+const UpdateFields = () =>{
+    balanceField.textContent = balance;
+    loanField.textContent = loanAmount;
+    payField.textContent = payBalance
+}
+
+loanButton.onclick = LoanButtonFunc;
+bankButton.onclick = BankButtonFunc;
+workButton.onclick = WorkButtonFunc;
+
+UpdateFields();
