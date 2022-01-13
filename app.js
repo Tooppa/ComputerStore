9168f1b58b54cc56ed60532f62dce7279d502a7e
Tomas Valkendorff <Tomvalken1@gmail.com>
2022-01-13 12:33:20 +0200

    connecting buttons

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
-const loanB = document.getElementById('loan');
-const bankB = document.getElementById('bank');
-const workB = document.getElementById('work');
+//gets the elements that contain buttons and fields
+const elements = document.getElementsByClassName('element');
+
+//gets the buttons from the elements
+const loanB = elements.item(0).querySelector('#loan');
+const bankB = elements.item(1).querySelector('#bank');
+const workB = elements.item(2).querySelector('#work');
+
+const balance = document.getElementById('balance');
 
 const LoanButton = () =>{
-
-}
+    console.log("woo");
+};
 const BankButton = () =>{
     
-}
+};
 const WorkButton = () =>{
     
-}
+};
+
+loanB.onclick = LoanButton;
